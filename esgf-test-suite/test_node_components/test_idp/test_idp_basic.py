b970b7015f288a98b02a6da0ed00e3e77a4a3cdb
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2017-11-15 16:42:54 +0100

    add esgf-orp web front end test

--- esgf-test-suite/test_node_components/test_idp/test_idp_basic.py
+++ esgf-test-suite/test_node_components/test_idp/test_idp_basic.py
@@ -8,7 +8,7 @@ import utils.naming as naming
 @attr ('basic')
 class TestWebFrontEnds(AbstractWebFrontEndTestClass):
   
-  _front_ends = ['esgf-slcs/admin']
+  _front_ends = ['esgf-slcs/admin', 'esgf-idp']
   
   def __init__(self):
     AbstractWebFrontEndTestClass.__init__(self, TestWebFrontEnds._front_ends,
