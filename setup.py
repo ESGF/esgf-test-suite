1a5263ad97ea71f1e99c0c96a94f6520941d35ed
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2017-11-17 14:14:07 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(name='esgf-test-suite',
-      version='0.2.3',
+      version='2.0.0',
       description='Nose scripts for ESGF integration test and validation',
       url='http://github.com/ESGF/esgf-test-suite',
       author='Nicolas Carenton',
       author_email='nicolas.carenton@ipsl.jussieu.fr',
+      maintainer='Sébastien Gardoll',
+      maintainer_email='sebastien.gardoll@ipsl.fr',
       license='IPSL',
       packages=find_packages(),
       install_requires=[
           'nose',
-          'pyOpenSSL==0.13.1',
-	  'MyProxyClient',
-	  'requests',
-	  'lxml',
-	  'splinter',
+          'pyopenssl',
+	        'MyProxyClient',
+	        'requests',
+	        'lxml',
+	        'splinter',
+          'nose-testconfig'
       ],
       zip_safe=False,
       include_package_data=True)
