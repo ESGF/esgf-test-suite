bf48e928ac9c76555b503bd0f6b81480427c4621
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2017-11-17 15:01:54 +0100

    disable create user test

--- esgf-test-suite/test/test_node_components/test_index/test_cog.py
+++ esgf-test-suite/test/test_node_components/test_index/test_cog.py
@@ -15,18 +15,20 @@ import utils.naming as naming
 @attr ('cog')
 class TestCreateUser(AbstractBrowserBasedTest):
   
-  def test_create_user(self):
-    
-    AbstractBrowserBasedTest.__init__(self)
+    def __init__(self):
+      
+      AbstractBrowserBasedTest.__init__(self)
 
-    usr = user.UserUtils()
-    usr.check_user_exists(globals.browser)
+#  def test_create_user(self):
+
+#    usr = user.UserUtils()
+#    usr.check_user_exists(globals.browser)
     
-    if(usr.user_exists):
-      raise SkipTest("User already exists")
+#    if(usr.user_exists):
+#      raise SkipTest("User already exists")
     
     # Create user
-    usr.create_user(globals.browser)
+#    usr.create_user(globals.browser)
     # Test output from create_user and eventually print error message
-    assert(isinstance(usr.response, list))
-    assert(usr.response[0] == naming.SUCCESS), usr.response
+#    assert(isinstance(usr.response, list))
+#    assert(usr.response[0] == naming.SUCCESS), usr.response
