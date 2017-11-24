859ce2cc29bc7f0a3cb19c6579fc96435cc087ba
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2017-11-24 16:48:57 +0100

    better error reportign

--- esgf-test-suite/test/test_node_components/test_idp/test_myproxy.py
+++ esgf-test-suite/test/test_node_components/test_idp/test_myproxy.py
@@ -6,6 +6,9 @@ from utils.abstract_myproxy_based_test import AbstractMyproxyBasedTest
 import utils.globals as globals
 from nose.plugins.attrib import attr
 
+from testconfig import config
+import utils.naming as naming
+
 @attr ('node_components')
 @attr ('idp')
 @attr ('myproxy')
@@ -16,21 +19,24 @@ class TestMyProxy(AbstractMyproxyBasedTest):
   
   def test_get_trustroots(self):
   # Test output from get_trustroots
-    assert(isinstance(globals.myproxy_utils.trustRoots, dict))
+    err_msg = "Unsupported trusted root certificate format '{0}'".format(globals.myproxy_utils.trustRoots)
+    assert(isinstance(globals.myproxy_utils.trustRoots, dict)), err_msg
     for fileName, fileContents in globals.myproxy_utils.trustRoots.items():
       if fileName.endswith('.0'):
         # test parsing certificate 
         cert = crypto.load_certificate(crypto.FILETYPE_PEM, fileContents)
-        assert(isinstance(cert, crypto.X509))
+        assert(isinstance(cert, crypto.X509)), "Unsupported certificate format '{0}'".format(cert)
         subj = cert.get_subject()
-        assert(subj)
+        err_msg = "Fail to get the trusted root certificates for '{0}'".format(config[naming.NODES_SECTION][naming.IDP_NODE_KEY])
+        assert(subj), err_msg
 
   def test_get_credentials(self):
     # Test output from get_trustroots
-    assert(isinstance(globals.myproxy_utils.credentials, tuple))
+    err_msg = "Unsupported credentials format '{0}'".format(globals.myproxy_utils.credentials)
+    assert(isinstance(globals.myproxy_utils.credentials, tuple)), err_msg
     cert = crypto.load_certificate(crypto.FILETYPE_PEM,
                  globals.myproxy_utils.credentials[0])
     key = crypto.load_privatekey(crypto.FILETYPE_PEM,
                globals.myproxy_utils.credentials[1])
-    assert(isinstance(cert, crypto.X509))
-    assert(isinstance(key, crypto.PKey))
+    assert(isinstance(cert, crypto.X509)), "Unsupported certificate format '{0}'".format(cert)
+    assert(isinstance(key, crypto.PKey)), "Unsupported cryptographic key format '{0}'".format(key)
