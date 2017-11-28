5b3241c4a86dd5b27926aacc988ce750be7c6685
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2017-11-28 10:31:31 +0100

    add basic testing for statistics api

---
+++ esgf-test-suite/test/test_node_components/test_data/test_stats.py
@@ -0,0 +1,25 @@
+from utils.abstract_web_frontend_test_class import AbstractWebFrontEndTestClass
+from nose.plugins.attrib import attr
+
+import utils.configuration as config
+
+@attr ('node_components')
+@attr ('data')
+@attr ('basic')
+@attr ('stats')
+class TestStatsFrontEnds(AbstractWebFrontEndTestClass):
+  
+  _stats_api = ['esgf-stats-api/cmip5/stats-by-space/xml',
+                'esgf-stats-api/cmip5/stats-by-dataset/xml',
+                'esgf-stats-api/cmip5/stats-by-experiment/xml',
+                'esgf-stats-api/cmip5/stats-by-model/xml',
+                'esgf-stats-api/cmip5/stats-by-variable/xml',
+                'esgf-stats-api/obs4mips/stats-by-space/xml',
+                'esgf-stats-api/obs4mips/stats-by-dataset/xml',
+                'esgf-stats-api/obs4mips/stats-by-realm/xml',
+                'esgf-stats-api/obs4mips/stats-by-source/xml',
+                'esgf-stats-api/obs4mips/stats-by-variable/xml']
+  
+  def __init__(self):
+    AbstractWebFrontEndTestClass.__init__(self, TestStatsFrontEnds._stats_api,
+                                          config.DATA_NODE_KEY)
