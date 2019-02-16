b35242f1e8a60153e9df616891d772aee7aa381d
Muryanto <muryanto1@ML-9516678.llnl.gov>
2019-02-15 16:09:16 -0800

    added a print debug in test_dl.py

--- esgf-test-suite/tests/test_dl.py
+++ esgf-test-suite/tests/test_dl.py
@@ -112,6 +112,8 @@ class TestDataDownload(AbstractBrowserBasedTest, AbstractMyproxyBasedTest):
     os.environ['X509_USER_PROXY'] = globals.myproxy_utils.credsfile
     os.environ['X509_CERT_DIR'] = globals.myproxy_utils.cacertdir
     command = ['globus-url-copy', '-b', url, TestDataDownload._DOWNLOADED_FILE_PATH]
+    print "globus-url-copy -b {u} {p}".format(u=url,
+                                              p=TestDataDownload._DOWNLOADED_FILE_PATH)
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     process.wait()
     stdout, stderr = process.communicate()
